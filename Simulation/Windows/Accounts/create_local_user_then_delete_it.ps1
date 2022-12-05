ea3b8f329b7b36700ee458ac7d8bd358041f0ffa
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-05 23:39:03 +0100

    Create create_local_user_then_delete_it.ps1

---
+++ Simulation/Windows/Accounts/create_local_user_then_delete_it.ps1
@@ -0,0 +1,12 @@
+#Create user
+$userName = "mthcht_test"
+#Create random key password
+$randomkey = -join ( (43..69) + (15..39) + (43..56) | Get-Random -Count 34 | % {[char]$_})
+#Convert password to SecureString so we can use it with the New-LocalUser function
+$Password = ConvertTo-SecureString $randomkey –AsPlainText –Force
+#Create the new user mthcht_test
+New-LocalUser -Name $userName -Password $Password -FullName "mthcht_test" -Description "Purpleteam Simulation"
+#Wait 10 seconds
+Start-Sleep -Seconds 10
+#Delete mthcht_test
+Remove-LocalUser -Name $userName
