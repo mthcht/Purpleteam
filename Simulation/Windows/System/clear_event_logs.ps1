2ba0385a1b5ba5705d740af0d27bab2b253422b5
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-06 00:35:35 +0100

    Create clear_event_logs.ps1

---
+++ Simulation/Windows/System/clear_event_logs.ps1
@@ -0,0 +1,22 @@
+$evtLogs = (Get-EventLog -List).Log
+
+$j = 0
+foreach($i in $evtLogs)
+{
+ Write-Host "$j : $i" 
+ $j++
+}
+
+Write-Host "Which eventlogs do you want to clear? (select a number)"
+$evtLogIndexes = Read-Host
+$evtLogNames = $evtLogs[$evtLogIndexes]
+Write-Host "Are you sure you want to clear $evtLogNames ? (y/n)"
+$confirm = Read-Host
+
+If ($confirm -eq "y") 
+{
+    Clear-EventLog -LogName $evtLogNames
+    Write-Host "Event logs $evtLogNames have been cleared!"
+} Else {
+    Write-Host "Event logs $evtLogNames have not been cleared!"
+}
