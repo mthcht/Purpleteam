0e49f0be283c50010c43c94da91efa4539e8a6c9
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-06 23:26:15 +0100

    Create kerberoasting_spn_scan.ps1

---
+++ Simulation/Windows/ActiveDirectory/kerberoasting_spn_scan.ps1
@@ -0,0 +1,29 @@
+# We import the ActiveDirectory module without the need to install it on the current computer, the dll has been extracted from a Windows 10 x64 with RSAT installed
+# technique used by real attackers
+Import-Module .\Microsoft.ActiveDirectory.Management.dll
+
+#Create a Directory Searh Object
+$searcher = New-Object System.DirectoryServices.DirectorySearcher
+
+#Set the search base to the current domain
+$searcher.SearchRoot = [System.DirectoryServices.DirectoryEntry] "LDAP://$((Get-ADDomain).DistinguishedName)"
+
+#Set the filter to search for all SPN's
+$searcher.Filter = "(&(objectCategory=computer)(serviceprincipalname=*))"
+
+#Execute the search
+$results = $searcher.FindAll()
+
+#Loop through the results
+foreach($result in $results)
+{
+    #Get the SPN's from each result
+    $spns = $result.Properties["serviceprincipalname"]
+    
+    #Loop through the SPN's
+    foreach($spn in $spns)
+    {
+        #Output the SPN
+        $spn
+    }
+}
