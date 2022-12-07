9e29298d8a5bf118681dc95cb2c61a1ab2905c3f
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-07 09:10:37 +0100

    Update test_ransom.ps1

--- Simulation/Windows/ransomware/test_ransom.ps1
+++ Simulation/Windows/ransomware/test_ransom.ps1
@@ -1,3 +1,14 @@
+<#
+- replace path variable in the script by the path folder containing the files you want to crypt.
+$path = "C:\Users\FIXME\Documents\test_ransom"
+
+Execute the powershell script: powershell -ExecutionPolicy Bypass -File '.\test_ransom.ps1'
+
+TODO:
+- Add arguments to set the path, static key, decrypt and crypt option
+#>
+
+
 function Invoke-AESEncryption {
     [CmdletBinding()]
     [OutputType([string])]
