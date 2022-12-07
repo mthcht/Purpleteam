ad3ed4634f4546659decd708c813adc69b86cd55
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-07 23:43:30 +0100

    Create simple_authentication_test_prompt.ps1

---
+++ Simulation/Windows/ActiveDirectory/simple_authentication_test_prompt.ps1
@@ -0,0 +1,18 @@
+
+# We import the ActiveDirectory module without the need to install it on the current computer, the dll has been extracted from a Windows 10 x64 with RSAT installed
+# technique used by real attackers
+Import-Module .\Microsoft.ActiveDirectory.Management.dll
+
+#Set credentials
+$cred = Get-Credential
+
+#Try authenticate user
+Try
+{
+    $user = Get-ADUser -Identity $cred.UserName -Credential $cred
+    Write-Host "User $($user.Name) authenticated successfully"    
+}
+Catch
+{
+    Write-Host "Authentication failed"
+}
