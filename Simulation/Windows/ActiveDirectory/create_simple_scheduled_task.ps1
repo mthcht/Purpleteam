7c001eca622bb1ac79568e5c26b1615f76e98b7f
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-08 21:25:44 +0100

    Create create_simple_scheduled_task.ps1

---
+++ Simulation/Windows/ActiveDirectory/create_simple_scheduled_task.ps1
@@ -0,0 +1,18 @@
+#First, we will define the necessary variables
+$action = 'C:\Windows\System32\cmd.exe'
+$arguments = ' /c "echo ola !"'
+$schedTaskName = 'Purpletem test'
+$triggerTime = (Get-Date).AddMinutes(1)
+
+#Now, we will create the scheduled task
+$schTask = New-ScheduledTaskAction -Execute $action -Argument $arguments
+$taskTrigger = New-ScheduledTaskTrigger -At $triggerTime -Once
+Register-ScheduledTask -Action $schTask -Trigger $taskTrigger -TaskName $schedTaskName -RunLevel Highest
+
+#Finally, we will confirm that the task was created
+$createdTask = Get-ScheduledTask -TaskName $schedTaskName
+if ($createdTask) {
+    Write-Output "Scheduled task '$schedTaskName' successfully created."
+} else {
+    Write-Output "Scheduled task '$schedTaskName' failed to be created."
+}
