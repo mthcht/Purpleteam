7cc0ef1ff2fb0115b053fe45245774a922451f03
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-09 23:27:06 +0100

    Create delete_rdp_history.ps1

---
+++ Simulation/Windows/System/delete_rdp_history.ps1
@@ -0,0 +1,13 @@
+#Erase RDP Connection History
+#This script will delete the registry entries and files related to recent RDP connections.
+
+#Get the current user
+$User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+
+#Delete registry entries
+Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" -Recurse
+Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" -Recurse
+
+#Delete files
+Remove-Item -Path "C:\Users\$User\Documents\Default.rdp" -Force
+Remove-Item -Path "C:\Users\$User\AppData\Local\Microsoft\TerminalServer Client\Cache\*" -Force
