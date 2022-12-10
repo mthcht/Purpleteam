ffc10f3662f2646c64a1eebde8f42d672ebbf833
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-10 19:16:31 +0100

    Update enumerate_files.ps1

--- Simulation/Windows/System/enumerate_files.ps1
+++ Simulation/Windows/System/enumerate_files.ps1
@@ -1,6 +1,6 @@
 # T1083 - File and Directory Discovery
 $csvPath = ".\enumerated_files.csv"
-$dirs = @("C:\Documents and Settings","C:\Program Files\","C:\Windows","C:\Program Files","C:\Program Files (x86)","C:\ProgramData","C:\Users","C:\Documents and Settings","C:\Program Files\Common Files","C:\WINDOWS\system32","C:\WINDOWS\SysWOW64","C:\WINDOWS\Temp","C:\WINDOWS\Prefetch","C:\WINDOWS\system32\drivers","C:\WINDOWS\SoftwareDistribution","C:\WINDOWS\System32\spool\drivers","C:\WINDOWS\system32\config","C:\WINDOWS\Microsoft.NET","C:\Documents and Settings\All Users","C:\Documents and Settings\All Users\Application Data","C:\Documents and Settings\Default User","C:\Documents and Settings\Default User\Application Data")
+$dirs = @("C:\Documents and Settings","C:\Windows","C:\Program Files","C:\Program Files (x86)","C:\ProgramData","C:\Users","C:\Documents and Settings","C:\Program Files\Common Files","C:\WINDOWS\system32","C:\WINDOWS\SysWOW64","C:\WINDOWS\Temp","C:\WINDOWS\Prefetch","C:\WINDOWS\system32\drivers","C:\WINDOWS\SoftwareDistribution","C:\WINDOWS\System32\spool\drivers","C:\WINDOWS\system32\config","C:\WINDOWS\Microsoft.NET","C:\Documents and Settings\All Users","C:\Documents and Settings\All Users\Application Data","C:\Documents and Settings\Default User","C:\Documents and Settings\Default User\Application Data")
 $data = @()
 
 foreach ($dir in $dirs)
