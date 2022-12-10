d324ee8c478ef602def9b404a0c10a202b320950
mthcht <75267080+mthcht@users.noreply.github.com>
2022-12-10 16:21:36 +0100

    Create enumerate_files.ps1

---
+++ Simulation/Windows/System/enumerate_files.ps1
@@ -0,0 +1,18 @@
+# T1083 - File and Directory Discovery
+$csvPath = ".\enumerated_files.csv"
+$dirs = @("C:\Documents and Settings","C:\Program Files\","C:\Windows","C:\Program Files","C:\Program Files (x86)","C:\ProgramData","C:\Users","C:\Documents and Settings","C:\Program Files\Common Files","C:\WINDOWS\system32","C:\WINDOWS\SysWOW64","C:\WINDOWS\Temp","C:\WINDOWS\Prefetch","C:\WINDOWS\system32\drivers","C:\WINDOWS\SoftwareDistribution","C:\WINDOWS\System32\spool\drivers","C:\WINDOWS\system32\config","C:\WINDOWS\Microsoft.NET","C:\Documents and Settings\All Users","C:\Documents and Settings\All Users\Application Data","C:\Documents and Settings\Default User","C:\Documents and Settings\Default User\Application Data")
+$data = @()
+
+foreach ($dir in $dirs)
+{
+    $files = Get-ChildItem -Path $dir -Recurse
+    foreach ($file in $files)
+    {
+        $obj = [pscustomobject]@{
+            File = $file.FullName
+        }
+        $data += $obj
+    }
+}
+
+$data | Export-Csv -Path $csvPath -NoTypeInformation
