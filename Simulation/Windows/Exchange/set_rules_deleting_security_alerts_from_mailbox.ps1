cfb8e6f9c2279190c7c9e3d2244a24cbead09a07
mthcht <mathieuchotplassot@gmail.com>
2022-12-21 00:35:08 +0100

    Update set_rules_deleting_security_alerts_from_mailbox.ps1

--- Simulation/Windows/Exchange/set_rules_deleting_security_alerts_from_mailbox.ps1
+++ Simulation/Windows/Exchange/set_rules_deleting_security_alerts_from_mailbox.ps1
@@ -9,7 +9,7 @@ Only for Exchange Servers
 
 $from_list = @("*sentinelone*","*trellix*","*mcafee*","*support*","*kaspersky*","*trendmicro*","*splunk*","*crowdstrike*","*@canary.tools*","alert*","soc@*","cert@*","*siem*","*soar*","soc-*","incident*","*cyber*","*defense*","*protect*","*detection*","*security*","*secops*")
 
-$subject_list = @("SOC *","[SOC*","SOC-*","*-SOC*","*Alert*","*Incident*","CERT *","High -","*Detection*","*Critical*","*malware*","*suspicious*","*phish*","*hack*","*Medium*","*suspect*","malicious","*incindent*","*alert*","*detection*","*protect*","*SentinelOne*","*crowdstrike*","*mcafee*","*trellix*","*kaspersky*","*EDR*","*antivirus*","*hack*","*bruteforce*","* scan *","*triggerred*")
+$subject_list = @("SOC *","[SOC*","SOC-*","*-SOC*","*Alert*","*Incident*","CERT *","High -","*Detection*","*Critical*","*malware*","*suspicious*","*phish*","*Medium*","*suspect*","malicious","*incindent*","*alert*","*detection*","*protect*","*SentinelOne*","*crowdstrike*","*mcafee*","*trellix*","*kaspersky*","*EDR*","*antivirus*","*hack*","*bruteforce*","* scan *","*triggerred*")
 
 foreach ($sender in $from_list)
 {
