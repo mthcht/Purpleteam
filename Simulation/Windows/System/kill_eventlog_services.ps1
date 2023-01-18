abcc3fb60b1448864572f72de8c1d037a2be91c1
mthcht <mathieuchotplassot@gmail.com>
2023-01-18 21:33:03 +0100

    Update kill_eventlog_services.ps1

--- Simulation/Windows/System/kill_eventlog_services.ps1
+++ Simulation/Windows/System/kill_eventlog_services.ps1
@@ -4,19 +4,40 @@
     T1562.003 - Impair Defenses: Impair Command History Logging
     Simple script to kill Windows Event Log services and Sysmon Services
 #>
-
 $services = @('EventLog','Sysmon64','Sysmon')
 foreach($svc in $services){
-    $Service = Get-WmiObject -Class win32_service -Filter "Name = `'$svc`'"
-    if($Service){
-        # Stop Services
-        Stop-Process -Id $Service.ProcessId -Force -PassThru -ErrorAction Stop
-        Set-Service -Name $service.Name -StartupType Disabled
-        sleep 4
-        
-        # Undo Actions
-        Set-Service -Name $service.Name -StartupType Automatic
-        Start-Service -Name $service.Name
-        Get-Service $Service.Name
+    try{
+        $Service = Get-WmiObject -Class win32_service -Filter "Name = `'$svc`'"
+        if($Service){
+            # Stop Services
+            if ((Get-Service $Service.Name).Status -eq "Running"){
+                Stop-Process -Id $Service.ProcessId -Force -PassThru -ErrorAction Stop -
+            }
+            Set-Service -Name $Service.Name -StartupType Disabled
+            if ((Get-Service $Service.Name).Status -ne "Running"){
+                Write-Host -ForegroundColor Green "Sucess: "$Service.Name"stopped"
+                sleep 4
+                # Undo Actions
+                Set-Service -Name $service.Name -StartupType Automatic
+                Start-Service -Name $service.Name
+                if ((Get-Service $Service.Name).Status -eq "Running"){
+                    Write-Host -ForegroundColor Green "Sucess: "$Service.Name"restarted" 
+                }
+                else{
+                    Write-Host -ForegroundColor Red "Error: "$Service.Name"could not be restarted..." 
+                }
+            }
+            else{
+                Write-Host -ForegroundColor Red "Error: "$Service.Name" is still running..." 
+            }
+   
+        }
+        else{
+            Write-Host -ForegroundColor Gray "Service $svc not found" 
+        }
+
+    }
+    catch{
+        Write-Host -ForegroundColor Red "Error: $_" 
     }
 }
