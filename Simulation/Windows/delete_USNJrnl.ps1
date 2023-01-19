82578a64f34839e2e3e0f04a4e7b1e4a1fc04737
mthcht <mathieuchotplassot@gmail.com>
2023-01-20 00:06:52 +0100

    Create delete_USNJrnl.ps1

---
+++ Simulation/Windows/delete_USNJrnl.ps1
@@ -0,0 +1,26 @@
+<#
+    T1070.004 - Indicator Removal: File Deletion
+    T1562.001 - Impair Defenses: Disable or Modify Tools
+    T1562.002 - Disable Windows Event Logging
+    Delete USNJrnl for all local drives with fsutil.exe (Anti forensic script)
+#>
+
+try{
+    $Drives = Get-WMIObject Win32_LogicalDisk | Select-Object -Property DriveType, DeviceID
+    foreach($Drive in $Drives)
+    {   
+        if($Drive.DriveType -eq 3){
+            &"fsutil.exe" usn deletejournal /D $Drive.DeviceID
+            $USNcontent = &"fsutil.exe" usn readJournal $Drive.DeviceID
+            if($USNcontent.Count -lt 10){
+                Write-Host -ForegroundColor Green "Success: USNJrnl Deleted for" $Drive.DeviceID
+            }
+            else{
+                Write-Host -ForegroundColor Yellow "Warning: There is still some data in USNJrnl that coul be erased, content of USNJrnl for" $Drive.DeviceID "`n $USNcontent "
+            }
+        }
+    }
+}
+catch{
+    Write-Host -ForegroundColor Red "`nError: $_"
+}
