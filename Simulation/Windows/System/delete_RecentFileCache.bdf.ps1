ba00b6b0f7708de295c74392840a34fb9e46574b
mthcht <mathieuchotplassot@gmail.com>
2023-01-20 00:30:54 +0100

    Create delete_RecentFileCache.bdf.ps1

---
+++ Simulation/Windows/System/delete_RecentFileCache.bdf.ps1
@@ -0,0 +1,31 @@
+<#
+    T1070.004 -  Indicator Removal: File Deletion 
+    T1070.009 -  Indicator Removal: Clear Persistence 
+    Simple script anti forensic: Remove RecentFileCache.bcf
+#>
+
+try{
+    $Drives = Get-WMIObject Win32_LogicalDisk | Select-Object -Property DriveType, DeviceID
+    foreach($Drive in $Drives)
+    {   
+        if($Drive.DriveType -eq 3){
+            $recentfilespath = [string]$Drive.DeviceID + "\Windows\AppCompat\Programs\RecentFileCache.bcf"
+            If (Test-Path $recentfilespath) {
+                Remove-Item -Path $recentfilespath -Force
+                If (!(Test-Path $recentfilespath)) {
+                    Write-Host -ForegroundColor Green "Sucess: RecentFileCache.bcf was deleted successfully"
+                }
+                Else {
+                    Write-Host "Unable to delete $recentfilespath"
+                }
+            }
+            Else {
+                Write-Host -ForegroundColor Yellow "$recentfilespath does not exist"
+            }
+            
+        }
+    }
+}
+catch{
+    Write-Host -ForegroundColor Red "Error: $_"
+}
