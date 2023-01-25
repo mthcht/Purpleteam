9264c590e27e28adf3f3352c2104412ebb7bf968
mthcht <mathieuchotplassot@gmail.com>
2023-01-25 13:59:24 +0100

    Update add_dnsclient_nrpt_rule.ps1

--- Simulation/Windows/System/add_dnsclient_nrpt_rule.ps1
+++ Simulation/Windows/System/add_dnsclient_nrpt_rule.ps1
@@ -1,26 +1,29 @@
-<# T1584.002 - Compromise Infrastructure: DNS Server
+<# 
+    T1584.002 - Compromise Infrastructure: DNS Server
+    
+   .SYNOPSIS
+   Creates a DNS Client NRPT rule.
 
-.SYNOPSIS
-Creates a DNS Client NRPT rule.
+   .DESCRIPTION
+   This script creates a DNS Client NRPT rule using the provided parameters. If no parameters are provided, the script will set default parameters for the namespace and nameservers.
 
-.DESCRIPTION
-This script creates a DNS Client NRPT rule using the provided parameters. If no parameters are provided, the script will set default parameters for the namespace and nameservers.
+   .PARAMETER Namespace
+   The namespace of the DNS Client NRPT rule.
 
-.PARAMETER Namespace
-The namespace of the DNS Client NRPT rule.
+   .PARAMETER NameServers
+   The nameservers of the DNS Client NRPT rule.
 
-.PARAMETER NameServers
-The nameservers of the DNS Client NRPT rule.
-
-.EXAMPLE
-.\Create-DNSCientNRPTRule.ps1 -Namespace "office.com" -NameServers "10.0.13.37"
- Create a NRPT rule that configures the server named 10.0.13.37 as a DNS server for the namespace office.com.
+   .EXAMPLE
+   .\Create-DNSCientNRPTRule.ps1 -Namespace "office.com" -NameServers "10.0.13.37"
+    Create a NRPT rule that configures the server named 10.0.13.37 as a DNS server for the namespace office.com.
 #>
 
 Param( 
     [Parameter(Mandatory=$false)][string]$Namespace = $Namespace,
     [Parameter(Mandatory=$false)][string]$NameServers = $NameServers
 )
+
+Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append
 
 # Set Default Parameters if no arguments are provided
 If (-not $Namespace) {$Namespace = "github.com"}
@@ -39,3 +42,5 @@ Catch
     Write-Warning -Message "An error occurred creating the DNS Client NRPT Rule for namespace: $Namespace and nameservers: $NameServers."
     Write-Warning -Message $_.Exception
 }
+
+Stop-Transcript
