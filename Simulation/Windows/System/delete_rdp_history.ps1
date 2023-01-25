295af287b158afad93afff68bbd217c45032f202
mthcht <mathieuchotplassot@gmail.com>
2023-01-25 14:13:50 +0100

    Update delete_rdp_history.ps1

--- Simulation/Windows/System/delete_rdp_history.ps1
+++ Simulation/Windows/System/delete_rdp_history.ps1
@@ -1,13 +1,24 @@
-# T1070.007 - Indicator Removal: Clear Network Connection History and Configurations
-#This script will delete the registry entries and files related to recent RDP connections.
+<# 
+    T1070.007 - Indicator Removal: Clear Network Connection History and Configurations
+    This script will delete the registry entries and files related to recent RDP connections.
+#>
 
-#Get the current user
-$User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append
 
-#Delete registry entries
-Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" -Recurse
-Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" -Recurse
+try{
+    #Get the current user
+    $User = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
 
-#Delete files
-Remove-Item -Path "C:\Users\$User\Documents\Default.rdp" -Force
-Remove-Item -Path "C:\Users\$User\AppData\Local\Microsoft\TerminalServer Client\Cache\*" -Force
+    #Delete registry entries
+    Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Default" -Recurse
+    Remove-Item -Path "Registry::HKEY_CURRENT_USER\Software\Microsoft\Terminal Server Client\Servers" -Recurse
+
+    #Delete files
+    Remove-Item -Path "C:\Users\$User\Documents\Default.rdp" -Force
+    Remove-Item -Path "C:\Users\$User\AppData\Local\Microsoft\TerminalServer Client\Cache\*" -Force
+}
+catch{
+    Write-Host -ForegroundColor Red "Error: $_"
+}
+
+Stop-Transcript
