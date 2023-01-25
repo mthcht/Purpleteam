8a2ba366f58915777b36bd1d4b77fd7f7fd016eb
mthcht <mathieuchotplassot@gmail.com>
2023-01-25 14:28:05 +0100

    Update enumerate_files.ps1

--- Simulation/Windows/System/enumerate_files.ps1
+++ Simulation/Windows/System/enumerate_files.ps1
@@ -1,5 +1,8 @@
 # T1083 - File and Directory Discovery
-$csvPath = ".\enumerated_files.csv"
+
+Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append
+
+$csvPath = "$env:tmp\enumerated_files.csv"
 $dirs = @("C:\Documents and Settings","C:\Windows","C:\Program Files","C:\Program Files (x86)","C:\ProgramData","C:\Users","C:\Documents and Settings","C:\Program Files\Common Files","C:\WINDOWS\system32","C:\WINDOWS\SysWOW64","C:\WINDOWS\Temp","C:\WINDOWS\Prefetch","C:\WINDOWS\system32\drivers","C:\WINDOWS\SoftwareDistribution","C:\WINDOWS\System32\spool\drivers","C:\WINDOWS\system32\config","C:\WINDOWS\Microsoft.NET","C:\Documents and Settings\All Users","C:\Documents and Settings\All Users\Application Data","C:\Documents and Settings\Default User","C:\Documents and Settings\Default User\Application Data")
 $data = @()
 
@@ -16,3 +19,5 @@ foreach ($dir in $dirs)
 }
 
 $data | Export-Csv -Path $csvPath -NoTypeInformation
+
+Stop-Transcript
