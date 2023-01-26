40a37866e70eb5f96c9978cef9b3c492bc1248c4
mthcht <mathieuchotplassot@gmail.com>
2023-01-26 11:40:05 +0100

    Update simple_tcp_subnet_and_port_scan.ps1

--- Simulation/Windows/Network/simple_tcp_subnet_and_port_scan.ps1
+++ Simulation/Windows/Network/simple_tcp_subnet_and_port_scan.ps1
@@ -1,4 +1,5 @@
-<# T1046 - Network Service Discovery
+<# 
+   T1046 - Network Service Discovery
    T1595.001 - Active Scanning: Scanning IP Blocks
    Simple powershell script to make a port scan or a subnet scan 
 #>
