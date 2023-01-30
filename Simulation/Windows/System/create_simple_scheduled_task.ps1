b75d430ab1fdfccd8f12a3f852b3ce2e69ca3d32
mthcht <mathieuchotplassot@gmail.com>
2023-01-30 12:32:19 +0100

    Update create_simple_scheduled_task.ps1

--- Simulation/Windows/System/create_simple_scheduled_task.ps1
+++ Simulation/Windows/System/create_simple_scheduled_task.ps1
@@ -1,26 +1,44 @@
 <#
-    T1222 - File and Directory Permissions Modification
+    T1053.005 - Scheduled Task/Job: Scheduled Task
+    T1033 - System Owner/User Discovery
+    Create a simple schedule task, wait for it to trigger and delete it
 #>
 
-Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append
+Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append -Force
 
-#First, we will define the necessary variables
-$action = 'C:\Windows\System32\cmd.exe'
-$arguments = ' /c "echo ola !"'
-$schedTaskName = 'Purpleteam test'
-$triggerTime = (Get-Date).AddMinutes(1)
+try{
+    # Define the necessary variables
+    $action = 'C:\Windows\System32\cmd.exe'
+    $arguments = ' /c "echo ola !&whoami&timeout 10"'
+    $schedTaskName = 'Purpleteam test'
+    $triggerTime = (Get-Date).AddSeconds(15)
 
-#Now, we will create the scheduled task
-$schTask = New-ScheduledTaskAction -Execute $action -Argument $arguments
-$taskTrigger = New-ScheduledTaskTrigger -At $triggerTime -Once
-Register-ScheduledTask -Action $schTask -Trigger $taskTrigger -TaskName $schedTaskName -RunLevel Highest
+    # Create the scheduled task
+    $schTask = New-ScheduledTaskAction -Execute $action -Argument $arguments -Verbose
+    $taskTrigger = New-ScheduledTaskTrigger -At $triggerTime -Once
+    Register-ScheduledTask -Action $schTask -Trigger $taskTrigger -TaskName $schedTaskName -RunLevel Highest -Verbose
 
-#Finally, we will confirm that the task was created
-$createdTask = Get-ScheduledTask -TaskName $schedTaskName
-if ($createdTask) {
-    Write-Output "Scheduled task '$schedTaskName' successfully created."
-} else {
-    Write-Output "Scheduled task '$schedTaskName' failed to be created."
+    # Confirm that the task was created
+    $createdTask = Get-ScheduledTask -TaskName $schedTaskName -Verbose
+    if ($createdTask) {
+        Write-Host -ForegroundColor Green "Sucess: Scheduled task '$schedTaskName' successfully created, waiting 15 seconds to trigger the scheduled task..."
+        Start-Sleep 15
+        #Remove Scheduled task created
+        Unregister-ScheduledTask -TaskName $schedTaskName -Confirm:$false -Verbose
+        if (-not (Get-ScheduledTask -TaskName $schedTaskName -ErrorAction SilentlyContinue)) {
+            Write-Host -ForegroundColor Green "Sucess: Simulation terminated, scheduled task '$schedTaskName' deleted"
+        }
+        else{
+            Write-Host -ForegroundColor Yellow "Warning: Scheduled task '$schedTaskName' is still on the system"
+        }
+    }
+    else {
+        Write-Host -ForegroundColor Red "Failed: Scheduled task '$schedTaskName' failed to be created."
+    }
+
+}
+catch{
+    Write-Host -ForegroundColor Red "`nError: $_"
 }
 
 Stop-Transcript
