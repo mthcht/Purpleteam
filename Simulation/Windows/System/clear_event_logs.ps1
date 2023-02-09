34842b06d734dd361c6eace1195e95991da47148
mthcht <mathieuchotplassot@gmail.com>
2023-02-09 12:30:33 +0100

    Update clear_event_logs.ps1

--- Simulation/Windows/System/clear_event_logs.ps1
+++ Simulation/Windows/System/clear_event_logs.ps1
@@ -1,30 +1,48 @@
 <#
     T1070.001 - Indicator Removal: Clear Windows Event Logs
+    Clear All Windows EventLogs (-ask for basic log selection)
 #>
 
-Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append
+param (
+    [Parameter(Mandatory=$false)]
+    [switch]$ask
+)
 
-$evtLogs = (Get-EventLog -List).Log
+Start-Transcript -Path "$env:tmp\simulation_traces.log" -Append -Force
 
-$j = 0
-foreach($i in $evtLogs)
-{
- Write-Host "$j : $i" 
- $j++
+try {
+    if ($ask -eq $true){
+        $evtLogs = (Get-EventLog -List).Log
+        Write-Host -ForegroundColor Cyan "Input Mode activated with -ask, asking user for basic log selections..."
+        $j = 0
+        foreach($i in $evtLogs){
+            Write-Host "$j : $i" 
+            $j++
+        }
+        Write-Host "Which eventlogs do you want to clear? (select a number)"
+        $evtLogIndexes = Read-Host
+        $evtLogNames = $evtLogs[$evtLogIndexes]
+        Write-Host "Are you sure you want to clear $evtLogNames ? (y/n)"
+        $confirm = Read-Host
+
+        if ($confirm -eq "y"){
+            Clear-EventLog -LogName $evtLogNames -Verbose
+            Write-Host "Event logs $evtLogNames have been cleared!"
+        }
+        else {
+            Write-Host "Event logs $evtLogNames have not been cleared!"
+        }
+    }
+    else {
+        $evtLogs = Get-WinEvent -ListLog * -Force -Verbose
+        foreach($event in $evtLogs){
+            Write-Host -ForegroundColor Cyan "Clearing log $($event.LogName)..."
+            wevtutil cl $event.LogName
+        }
+    }
 }
-
-Write-Host "Which eventlogs do you want to clear? (select a number)"
-$evtLogIndexes = Read-Host
-$evtLogNames = $evtLogs[$evtLogIndexes]
-Write-Host "Are you sure you want to clear $evtLogNames ? (y/n)"
-$confirm = Read-Host
-
-If ($confirm -eq "y") 
-{
-    Clear-EventLog -LogName $evtLogNames
-    Write-Host "Event logs $evtLogNames have been cleared!"
-} Else {
-    Write-Host "Event logs $evtLogNames have not been cleared!"
+catch {
+    Write-Host -ForegroundColor Red "Error: $_"
 }
 
 
