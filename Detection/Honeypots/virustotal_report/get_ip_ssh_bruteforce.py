af5d6c6db1d3b520448a90691ab73d73406a9d43
mthcht <mathieuchotplassot@gmail.com>
2023-04-02 04:41:04 +0200

    Update get_ip_ssh_bruteforce.py

--- Detection/Honeypots/virustotal_report/get_ip_ssh_bruteforce.py
+++ Detection/Honeypots/virustotal_report/get_ip_ssh_bruteforce.py
@@ -1,14 +1,32 @@
 #!/bin/python3
-# work in progress
 
 import csv
 import os
 import re
 import gzip
-log_dir = "/var/log/"
-honeydir = "/opt/virustotal_report/ssh/bf_list.csv"
+from datetime import datetime
 
+
+current_year = datetime.now().year
+log_dir = "/var/log/"
 result=[]
+
+def month_to_number(month_name):
+    month_dict = {
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12"
+    }
+    return month_dict.get(month_name, "N/A")
 
 def extract():
     for log_line in log_file:
@@ -18,9 +36,10 @@ def extract():
             ip_address = re.findall(r'[0-9]+(?:\.[0-9]+){3}', log_line)
             date_time_day = re.compile(r'^.+?(?=[0-9])(.+?(?=\s))')
             date_time_month = re.findall(r'^.+?(?=\s)', log_line)
+            date_time_month = ' '.join(date_time_month)
             date_time_day = date_time_day.match(log_line)
             if ip_address:
-                result.append((date_time_day.group(1), ip_address[0]))
+                result.append(("[{}/{}/{}]".format(current_year,month_to_number(date_time_month),date_time_day.group(1)),ip_address[0]))
 
 for filename in os.listdir(log_dir):
     if filename.startswith("auth"):
@@ -38,7 +57,7 @@ for filename in os.listdir(log_dir):
 
 result_dict = {}
 csv_dict = {}
-with open(honeydir,'r') as f_input:
+with open('/root/bf_list.csv','r') as f_input:
     csv_input = csv.reader(f_input)
     first_char = f_input.read(1)
     if first_char:
@@ -52,17 +71,20 @@ for num,ip in result:
         result_dict[ip] = num
 for ip in csv_dict:
     if ip not in result_dict:
-        result_dict[ip] = csv_dict[ip]
+        if ip.startswith("ip") or ip.startswith("date"):
+            pass
+        else:
+            result_dict[ip] = csv_dict[ip]
 print('final result:')
 print(list(result_dict.items()))
 print(result_dict)
-with open(honeydir,'w') as f_output:
+with open('/root/bf_list.csv','w') as f_output:
     csv_output = csv.writer(f_output)
     csv_output.writerow(['ip_address','date'])
     for ip,date in result_dict.items():
         csv_output.writerow([ip,date])
 
-with open(honeydir,'r') as f_input:
+with open('/root/bf_list.csv','r') as f_input:
     csv_reader = csv.DictReader(f_input)
     line_count = 0
     for row in csv_reader:
